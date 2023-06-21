e1d45c23f531244ffee4760fc586886ff0960b52
shkmzzh <3081764857@qq.com>
2023-06-21 22:17:32 +0800

    init

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+// 创建 express 的服务器实例
+const app = express()
+
+
+app.listen(7758, function () {
+    console.log('api server running at http://127.0.0.1:7758')
+})
